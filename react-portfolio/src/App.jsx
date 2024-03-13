3327e17dbca2139f05473b4c93d494fb18405aa3
Dillon Awes <dillonawes@gmail.com>
2024-03-13 15:04:24 -0500

    hardcode projects json

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
       <Experience />
     </div>
   );
