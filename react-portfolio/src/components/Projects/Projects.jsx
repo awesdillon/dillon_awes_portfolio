cbe118bc70f08b11d51f362c4de4888b539b3adc
Dillon Awes <dillonawes@gmail.com>
2024-03-13 15:18:59 -0500

    projects logic

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import projects from "../../data/projects.json";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section>
+      <h2>Projects</h2>
+      <div>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
