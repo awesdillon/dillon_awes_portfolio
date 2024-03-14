d5416590ed5918707ae9fb1ec1ee4658c7a6c370
Dillon Awes <dillonawes@gmail.com>
2024-03-14 13:14:28 -0500

    contact section

---
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Let's connect!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:dillonawes@gmail.com">dillonawes@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/dillon-awes-84a16128b/">
+            LinkedIn
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/awesdillon">Github</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Contact;
